b655c22b40aed751191b3729eab0e9f9367bbea1
Matt <hallmatthewd12@gmail.com>
2021-01-08 13:56:25 -0500

    First Init
    Done with section 3 and part into section 4

---
+++ Learning C/Learning C/Program.cs
@@ -0,0 +1,75 @@
+﻿// Programmer: Matthew Hall
+// Date: 1.7.2021
+// Program: Learning how to code with C#
+
+/*Resources:
+
+Tutorial Video - 
+https://www.youtube.com/watch?v=GhQdlIFylQ8
+*/
+
+/* Video Progress - 
+    Section 3 (Drawing a Shape) Done 1/8/2021 @ 1:12 pm
+    Section 4 (Variables) 18:06
+
+
+// This is how to do a single line comment
+
+/* This is how to do a 
+multi line comment */
+
+
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+
+
+
+// Tutorial Part 2
+namespace Learning_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("There once was a man named George");
+            Console.WriteLine("He was 70 years old");
+            Console.WriteLine("He really liked the name George");
+            Console.WriteLine("But he didn't line being 70");
+
+            Console.ReadLine(); // Keeps the console open instead of closing it immediately
+        }
+    }
+}
+
+
+
+/*
+// Tutorial Part 1
+namespace Learning_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Drawing a simple triangle to learn how to write lines
+            Console.WriteLine("   /|");
+            Console.WriteLine("  / |");
+            Console.WriteLine(" /  |");
+            Console.WriteLine("/___|");
+
+            Console.ReadLine(); // Keeps the console open instead of closing it immediately
+        }
+    }
+}
+*/
